5ec9967fd30a1f01592ae1278daf0efc33bb42e6
Christian Thaler <christian.thaler@tes-dst.com>
2022-10-27 16:01:11 +0200

    update documentation

--- README.md
+++ README.md
@@ -1,26 +1,9 @@
-# libdrm-gman
-GMAN (Gfx Manager) user space library for TES EvalKits. Do NOT use in
-production. EvalKit only!
+# GMAN - Graphics Memory Manager
 
-This library implements the GMAN DRM module user space interface for user
-space applications.
-Its purpose is to create, map and get the physical address of simple BOs
-(Buffer Objects).
+This library gives convenient access to the functionality of the GMAN kernel module.
+Its purpose is to provide DRM backed video memory to user space applications for
+use with baremetal driver.
 
-## Example
-A simple example that opens the device and allocates 4096 bytes:
+## API
 
-```
-struct gman_device *gman;
-int fd;
-struct gman_bo *bo;
-void *map;
-
-fd = drmOpen("tes-gman", NULL);
-gman = gman_device_new(fd);
-
-bo = gman_bo_new(gman, 4096, GMAN_BO_UNCACHED);
-map = gman_bo_map(bo);
-
-drmClose(gman_device_fd(gman));
-```
+gman-drmif.h
--- gman_drmif.h
+++ gman_drmif.h
@@ -1,3 +1,8 @@
+/**
+ * Title: GMAN - Graphics Manager
+ *
+ * A graphics memory manager for baremetal drivers.
+ */
 #ifndef __GMAN_DRMIF_H__
 #define __GMAN_DRMIF_H__
 
@@ -5,66 +10,217 @@
 #include <xf86drm.h>
 #include <stdint.h>
 
-
-struct gman_device;
-struct gman_stream;
-struct gman_bo;
-
-typedef uint64_t gman_dma_addr_t;
-
-/* device functions:
+/**
+ * Group: Types
  */
 
+/**
+ * Type: gman_device
+ * 
+ * GMAN device handle.
+ */
+struct gman_device;
+
+/**
+ * Type: gman_bo
+ * 
+ * Buffer object handle.
+ */
+struct gman_bo {
+	int dummy;
+};
+
+/**
+ * Type: gman_dma_addr_t
+ * 
+ * Physical address as used by HW devices.
+ */
+typedef uint64_t gman_dma_addr_t;
+
+/* Group: Device functions
+ */
+
+/**
+ * function: gman_device_new
+ *
+ * Create new GMAN device handle.
+ *
+ * parameters:
+ *  fd - file descriptor of GMAN device
+ *
+ * The file descriptor fd identifies the GMAN device. It is a file descriptor
+ * of an opened GMAN device.
+ *
+ * returns:
+ *  GMAN device handle
+ */
 struct gman_device *gman_device_new(int fd);
+
+/**
+ * function: gman_device_del
+ *
+ * Delete GMAN device handle.
+ *
+ * parameters:
+ *  dev - GMAN device handle created with <gman_device_new>
+ *
+ * Gracefully shut down the GMAN device and free the device handle.
+ */
 void gman_device_del(struct gman_device *dev);
+
+/**
+ * function: gman_device_fd
+ *
+ * Return the file descriptor used to create the given GMAN handle.
+ *
+ * parameters:
+ *  dev - GMAN device handle created with <gman_device_new>
+ *
+ * Depending on the platform, the file descriptor used during creation is
+ * also required during clean up (e.g. calling close()). For convenience,
+ * this function returns the file descriptor used to create the given handle.
+ *
+ * returns:
+ *  file decriptor used to create the given handle
+ */
 int gman_device_fd(struct gman_device *dev);
 uint32_t *gman_device_mem_io(struct gman_device *dev);
 
 
-/* buffer-object functions:
+/* Group: Buffer-Object functions:
  */
 
+/**
+ * Function: gman_bo_new
+ *
+ * Create a new BO of the given size.
+ *
+ * Parameters:
+ *  dev - GMAN device handle created with <gman_device_new>
+ *
+ * Depending on the platform, the file descriptor used during creation is
+ * also required during clean up (e.g. calling close()). For convenience,
+ * this function returns the file descriptor used to create the given handle.
+ *
+ * Returns:
+ *  File decriptor used to create the given handle
+ */
+/* allocate a new (un-tiled) buffer object */
 struct gman_bo *gman_bo_new(struct gman_device *dev,
 		uint32_t size, uint32_t flags);
+
+/**
+ * Function: gman_bo_map
+ *
+ * Maps a BO to CPU address space.
+ *
+ * Parameters:
+ *  bo - BO that will be mapped
+ *
+ * returns:
+ *  Pointer to the mapped BO data or NULL
+ */
 void *gman_bo_map(struct gman_bo *bo);
+
+/**
+ * Function: gman_bo_dma_addr
+ *
+ * Get a DMA address for the given BO.
+ * 
+ * The DMA address is platform dependent. No assumptions can be made.
+ *
+ * Parameters:
+ *  bo - the BO whose DMA address will be a acquired
+ *
+ * returns:
+ *  A DMA address for the given BO.
+ */
 gman_dma_addr_t gman_bo_dma_addr(struct gman_bo *bo);
+
+/**
+ * Function: gman_bo_del
+ *
+ * Delete a BO created with <gman_bo_new>.
+ * 
+ * The memory used by the BO is freed.
+ *
+ * Parameters:
+ *  p - the BO to delete.
+ */
 void gman_bo_del(struct gman_bo *bo);
+
+/**
+ * Function: gman_bo_from_dmabuf
+ *
+ * Create a BO from a dmabuf.
+ * 
+ * The dmabuf is referenced by a file descriptor. This can be used on Linux
+ * platforms to integrate devices with an actual Linux driver.
+ *
+ * Parameters:
+ *  dev - the GMAN device handle
+ *  fd - the file descriptor of the dmabuf
+ *
+ * returns:
+ *  a BO handle or NULL.
+ */
 struct gman_bo *gman_bo_from_dmabuf(struct gman_device *dev, int fd);
 
 
-/* debug functions:
+/* Debug functions are not supported yet.
  */
-
 int gman_debug_selftest(struct gman_device *dev);
 void gman_debug_reset(struct gman_device *dev);
 
 
-/* stream functions:
- */
-
+/* Streams are not supported yet. */
 struct gman_stream {
 	void *stream;           /* ptr to stream data */
 	void *jump;             /* ptr to jump address at stream's end */
 };
 
-struct gman_bo {
-	int dummy;
-};
-
-struct gman_stream *gman_stream_new(struct gman_device *device);
+struct gman_stream *gman_stream_new(struct gman_device *dev);
 void gman_stream_del(struct gman_stream *stream);
-void gman_stream_append_bo(struct gman_device *device, struct gman_bo *bo);
+void gman_stream_append_bo(struct gman_device *dev, struct gman_bo *bo);
 void gman_stream_flush(struct gman_stream *stream);
 int gman_stream_finish(struct gman_stream *stream);
 uint32_t gman_stream_timestamp(struct gman_stream *stream);
 
-/* Display Interface support
+/* Group: Display Interface functions
  */
 #ifndef fbo_t
+/* Anonymous declaration if needed. fbo_t is platform dependent and defined by
+ * the used DI implementation. */
 typedef struct _fbo_t fbo_t;
 #endif
 
-extern fbo_t * gman_createfbo(struct gman_device *device, struct gman_bo *bo, unsigned int width, unsigned int height, unsigned int pitch, int bpp);
-extern void gman_deletefbo(struct gman_device *device, fbo_t *fbo);
+/**
+ * Function: gman_createfbo
+ *
+ * Create an FBO for use with the DI display interface.
+ *
+ * Parameters:
+ *  dev - a GMAN device handle
+ *  bo - a BO used as frame buffer.
+ *  width - frame buffer's width
+ *  height - frame buffer's height
+ *  pitch - frame buffer's pitch in pixels
+ * 	bpp - bytes per pixel
+ *
+ * returns:
+ *  fbo handle or NULL
+ */
+fbo_t * gman_createfbo(struct gman_device *dev, struct gman_bo *bo, unsigned int width, unsigned int height, unsigned int pitch, int bpp);
+
+/**
+ * Function: gman_deletefbo
+ *
+ * Delete an FBO created with <gman_createfbo>.
+ *
+ * Parameters:
+ *  dev - a GMAN device handle
+ *  fbo - the FBO to delete
+ */
+void gman_deletefbo(struct gman_device *dev, fbo_t *fbo);
 
 #endif
